9ee5a672021f6e4856ede4b7b3ef753c7c708892
Xiexie <xie@123.com>
2022-10-27 20:49:50 +0800

    2022.10.27 新建的WebServer模拟实现Tomcat

---
+++ V1/src/main/java/com/webserver/core/WebServerApplication.java
@@ -0,0 +1,13 @@
+package com.webserver.core;
+
+/*
+*   WebServerApplication是一个web容器，实现Tomcat的基础功能
+*   web容器主要有两个功能:
+*   1、管理部署网络应用
+*   2、处理客户端的请求，建立TCP连接和使用HTTP协议进行交互，使得浏览器可以调用
+* 部署在网络应用中的功能
+*   网络应用:每个网络应用通常都包含网页、素材、逻辑处理代码等，就是我们俗称的一个网站
+*
+ */
+public class WebServerApplication {
+}
